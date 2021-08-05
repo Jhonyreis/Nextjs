e0682c5031c625f819d4a3eedd5c32492655e53f
Jonatas Reis <studioi9ideias@hotmail.com>
2021-08-04 21:03:03 -0300

    preparando ambiente pre-front página login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <div className='w-screen h-screen bg-gray-50 flex justify-center'>
+      <div className='bg-red-100 w-full h-full max-w-screen-lg grid grid-cols-2'>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          main
+        </main>
+      </div>
+    </div>
+  )
+}
